5f3d67f30e52979f82c5cd1a658ae6a03f757453
Ariful islam <arifulbgt4@gmail.com>
2023-12-08 10:26:08 +0600

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div>NotFound hello 1</div>;
+};
+
+export default NotFound;
---
+++ src/components/Texts.tsx
@@ -0,0 +1,8 @@
+function Texts({ children }: any) {
+  return <h2>Hello {children}</h2>;
+}
+
+export default Texts;
+
+export const a = 10;
+export const b = () => {};
